7d7d161c7b860b0eb28e8630e7d25f34a72535b3
Drake Smith <drake-smith@uiowa.edu>
2024-02-25 14:21:27 -0600

    Added GraphImages folder

--- studyDuration.py
+++ studyDuration.py
@@ -6,39 +6,17 @@ from dataInitialization import description
 covid = pd.read_csv("Data/covid.csv")
 breastCancer = pd.read_csv("Data/breastCancer.csv")
 
-[
-    "NCT Number",
-    "Study Title",
-    "Study URL",
-    "Acronym",
-    "Study Status",
-    "Brief Summary",
-    "Study Results",
-    "Conditions",
-    "Interventions",
-    "Primary Outcome Measures",
-    "Secondary Outcome Measures",
-    "Other Outcome Measures",
-    "Sponsor",
-    "Collaborators",
-    "Sex",
-    "Age",
-    "Phases",
-    "Enrollment",
-    "Funder Type",
-    "Study Type",
-    "Study Design",
-    "Other IDs",
-    "Start Date",
-    "Primary Completion Date",
-    "Completion Date",
-    "First Posted",
-    "Results First Posted",
-    "Last Update Posted",
-    "Locations",
-    "Study Documents",
-    "Study Length",
-]
+# ['Unnamed: 0', 'NCT Number', 'Study Status', 'Study Results',
+#        'Conditions', 'Interventions', 'Primary Outcome Measures',
+#        'Secondary Outcome Measures', 'Other Outcome Measures', 'Sponsor',
+#        'Collaborators', 'Sex', 'Age', 'Phases', 'Enrollment', 'Funder Type',
+#        'Study Type', 'Study Design', 'Other IDs', 'Start Date',
+#        'Primary Completion Date', 'Completion Date', 'First Posted',
+#        'Results First Posted', 'Last Update Posted', 'Locations',
+#        'Study Documents', 'Has Secondary Outcome', 'Has More Than 2 Outcomes',
+#        'Has Collaborators', 'Has Study Docs', 'Study Length']
+
+print(covid.columns)
 print("Covid")
 print(description(covid))
 print("")
@@ -57,4 +35,14 @@ sb.displot(data=combined_df, x="Study Length", hue="Dataset", kind="kde")
 plt.title("Study Length KDE")
 plt.xlabel("Study Length (days)")
 plt.ylabel("Density")
+plt.tight_layout()
+plt.savefig("GraphImages/bothStudyLengthDensity.png")
 plt.show()
+
+for df in [covid, breastCancer]:
+    for hue in [""]:
+        sb.displot(data=df, x="Study Length", hue="Phases", kind="kde")
+        plt.title("Study Length KDE")
+        plt.xlabel("Study Length (days)")
+        plt.ylabel("Density")
+        plt.show()
