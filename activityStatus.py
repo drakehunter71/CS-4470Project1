21539c894bb9d2aafbd486fe37857cefd8078347
Alec Langowski <alangowski@uiowa.edu>
2024-02-27 22:20:16 -0600

    Intervention Type Analysis Complete

--- activityStatus.py
+++ activityStatus.py
@@ -6,23 +6,23 @@ import matplotlib.pyplot as plt
 c = pd.read_csv("Data/covid.csv")
 bc = pd.read_csv("Data/breastCancer.csv")
 
-cCount = pd.DataFrame(columns=["Study Status", "count", "disease"])
-bcCount = pd.DataFrame(columns=["Study Status", "count", "disease"])
+cCount = pd.DataFrame(columns=["Study Status", "count", "Disease"])
+bcCount = pd.DataFrame(columns=["Study Status", "count", "Disease"])
 
 order = ["NOT_YET_RECRUITING", "RECRUITING", "ENROLLING_BY_INVITATION", "ACTIVE_NOT_RECRUITING", "cOMPLETED", "TERMINATED",
          "SUSPENDED", "WITHDRAWN", "UNKNOWN"]
 
 temp = c["Study Status"].value_counts().reset_index()
-temp["disease"] = "Covid"
+temp["Disease"] = "Covid"
 for cat in order:
     cCount = pd.concat([cCount, temp[temp["Study Status"] == cat]])
 
 bcCount = bc["Study Status"].value_counts().reset_index()
-bcCount["disease"] = "Breast Cancer"
+bcCount["Disease"] = "Breast Cancer"
 
 count1 = pd.concat([cCount, bcCount])
 
-sns.barplot(count1, x="Study Status", y="count", hue="disease", palette="flare")
+sns.barplot(count1, x="Study Status", y="count", hue="Disease", palette="flare")
 plt.title("Distribution of Studies by Status")
 plt.xticks(rotation=45)
 plt.show()
--- trialType.py
+++ trialType.py
@@ -5,3 +5,27 @@ import matplotlib.pyplot as plt
 
 c = pd.read_csv("Data/covid.csv")
 bc = pd.read_csv("Data/breastCancer.csv")
+
+def total(interventions):
+    l = interventions.split("|")
+    for intervention in l:
+        t = intervention.split(":")[0]
+        if t not in count.keys():
+            count[t] = 1
+        else:
+            count[t] = count[t] + 1
+
+count = {}
+c["Interventions"].apply(total)
+cCount = pd.DataFrame({"Intervention" : list(count.keys()), "count" : list(count.values()), "Disease" : ["Covid"] * len(list(count.keys()))})
+
+count = {}
+bc["Interventions"].apply(total)
+bcCount = pd.DataFrame({"Intervention" : list(count.keys()), "Count" : list(count.values()), "Disease" : ["Breast cancer"] * len(list(count.keys()))})
+
+counts = pd.concat([cCount, bcCount])
+
+sns.barplot(counts, x="Intervention", y="count", hue="Disease", palette="flare")
+plt.title("Distribution of Studies by Intervention Type(s)")
+plt.xticks(rotation=45)
+plt.show()
